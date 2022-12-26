e872cdb858f1311cf911986c6f9278ebce13840b
Stephen-Ibe <saintkalson@gmail.com>
2022-12-26 10:28:02 +0100

    added middleware to tourendpoint

--- middlewares/protected.js
+++ middlewares/protected.js
@@ -1,1 +1,18 @@
-exports.isProtected = catchAsync((req, res, next) => {});
+const ErrorHandler = require('../utils/errorHandler');
+const { catchAsync } = require('../utils/errorHandler');
+
+exports.isProtected = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new ErrorHandler('Unauthorized! Please log in', 401));
+  }
+
+  next();
+});
